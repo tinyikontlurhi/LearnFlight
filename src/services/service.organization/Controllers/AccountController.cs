21f072d1e7bd26a18b8124078c948c2df1a3eda4
ThatoLesetla <tlesetla6198@gmail.com>
2021-09-29 15:25:37 +0200

    Add account controller and update authentication controller

--- src/services/service.organization/Controllers/AccountController.cs
+++ src/services/service.organization/Controllers/AccountController.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using service.organization.data;
-using service.organization.dtos;
-using service.organization.services;
+using service.organization.models;
 
 namespace service.organization.Controllers
 {
@@ -15,29 +15,94 @@ namespace service.organization.Controllers
     public class AccountController : ControllerBase
     {
         private readonly DataContext _context;
-        private readonly OrganizationService _organizationService;
 
-        public AccountController(DataContext context, OrganizationService organizationService)
+        public AccountController(DataContext context)
         {
             _context = context;
-            _organizationService = organizationService;
         }
 
-        [HttpPost("login")]
-        public async Task<ActionResult<PayloadDTO>> login(LoginDTO loginDTO)
+        // GET: api/Account
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Account>>> Getaccount()
         {
-            if (!ModelState.IsValid)
-                return BadRequest();
+            return await _context.account.ToListAsync();
+        }
 
-            var organization = await _organizationService.Login(loginDTO);
+        // GET: api/Account/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Account>> GetAccount(int id)
+        {
+            var account = await _context.account.FindAsync(id);
 
-            if (organization == null)
+            if (account == null)
             {
-                return Unauthorized("Invalid username or password");
+                return NotFound();
             }
 
-            return Ok(organization);
+            return account;
+        }
 
+        // PUT: api/Account/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAccount(int id, Account account)
+        {
+            if (id != account.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(account).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AccountExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Account
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Account>> PostAccount(Account account)
+        {
+            _context.account.Add(account);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAccount", new { id = account.Id }, account);
+        }
+
+        // DELETE: api/Account/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAccount(int id)
+        {
+            var account = await _context.account.FindAsync(id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            _context.account.Remove(account);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AccountExists(int id)
+        {
+            return _context.account.Any(e => e.Id == id);
         }
     }
 }
