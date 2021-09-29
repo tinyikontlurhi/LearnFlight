e9966c398f8136fe1b9d6ea9ea528ae67fe0d83f
ThatoLesetla <tlesetla6198@gmail.com>
2021-09-29 15:32:14 +0200

    Add Contact controller

---
+++ src/services/service.organization/Controllers/ContactController.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using service.organization.data;
+using service.organization.models;
+
+namespace service.organization.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ContactController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Contact
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Contact>>> Getcontact()
+        {
+            return await _context.contact.ToListAsync();
+        }
+
+        // GET: api/Contact/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Contact>> GetContact(int id)
+        {
+            var contact = await _context.contact.FindAsync(id);
+
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return contact;
+        }
+
+        // PUT: api/Contact/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutContact(int id, Contact contact)
+        {
+            if (id != contact.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(contact).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContactExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Contact
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Contact>> PostContact(Contact contact)
+        {
+            _context.contact.Add(contact);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetContact", new { id = contact.Id }, contact);
+        }
+
+        // DELETE: api/Contact/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteContact(int id)
+        {
+            var contact = await _context.contact.FindAsync(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            _context.contact.Remove(contact);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ContactExists(int id)
+        {
+            return _context.contact.Any(e => e.Id == id);
+        }
+    }
+}
